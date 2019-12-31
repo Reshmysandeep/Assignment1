81e9810ca8e907b2a5de2c6aa2a149788828025d
Reshmy <reshmy.p.p@gmail.com>
2019-12-31 12:42:28 +0400

    Add files via upload

--- readcustomerinfo.js
+++ readcustomerinfo.js
@@ -8,6 +8,9 @@ const outPath=path.join(__dirname,'./customer-data.json')
 
 
 csvjson().fromFile(filepath).then((jsonobj)=>{
-    let data = JSON.stringify(jsonobj);
-    fs.writeFileSync(outPath, data);
+    let data = JSON.stringify(jsonobj,null,4);
+    fs.writeFileSync(outPath, data,'utf8',function(err){
+        console.log("An error occured while writing JSON Object to File.");
+        return console.log(err);
+    });
 })
